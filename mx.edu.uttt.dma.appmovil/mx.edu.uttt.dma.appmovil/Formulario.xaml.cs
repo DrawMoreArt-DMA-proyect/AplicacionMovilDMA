d449abcfa0f6688169f8a1e82951a74e4b5ad691
israel30jesus <jesusisraelvaladezalvarado6@gmail.com>
2021-11-24 21:17:00 -0600

    Agregando las vistas del login y el registro

---
+++ mx.edu.uttt.dma.appmovil/mx.edu.uttt.dma.appmovil/Formulario.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mx.edu.uttt.dma.appmovil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Formulario : ContentPage
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+
+        void NavigateButtonClicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
