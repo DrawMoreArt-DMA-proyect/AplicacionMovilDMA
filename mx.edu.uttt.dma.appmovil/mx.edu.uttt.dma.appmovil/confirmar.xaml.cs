d814294e7a9068b02b8e785108982493a3967d9d
Hector-Antonio-Ugalde-Quiroz <hectoruq18@gmail.com>
2021-12-01 12:30:46 -0600

    creacion de vista confirmar correo

---
+++ mx.edu.uttt.dma.appmovil/mx.edu.uttt.dma.appmovil/confirmar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mx.edu.uttt.dma.appmovil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class confirmar : ContentPage
+    {
+        public confirmar()
+        {
+            InitializeComponent();
+        }
+        void regresarclicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+    }
+}
